21d6ac9afe7ac0eac940b115e1240a0f2526129d
PLKuba <major.plays96@gmail.com>
2025-07-28 11:15:32 +0200

    adjust `CORS

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ankietalogopedyczna.pl",
+        "https://www.ankietalogopedyczna.pl"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
